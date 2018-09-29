a9b058f98a82fa13100f8eac1c4a609bad73b8ea
Plinio Mabesi <pliniomabesi@gmail.com>
2018-09-29 18:27:50 -0300

    Truncamento de Course_User em Seeders

--- database/seeds/CoursesTableSeeder.php
+++ database/seeds/CoursesTableSeeder.php
@@ -12,11 +12,12 @@ class CoursesTableSeeder extends Seeder
      */
     public function run()
     {
+      DB::table('course_user')->truncate();
       Course::truncate();
       factory(App\Course::class, 15)->create();
 
       Course::find(1)->users()->attach([2,3,6,9]);
       Course::find(2)->users()->attach([3,5,7]);
-      
+
     }
 }
